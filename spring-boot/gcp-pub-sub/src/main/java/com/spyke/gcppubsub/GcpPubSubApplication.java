077b52d072dc2daf0c90cf76a25469a72e5bd72b
fabbo-repo <fscherle98@gmail.com>
2023-06-29 11:29:07 +0200

    Pub sub spyke created

---
+++ spring-boot/gcp-pub-sub/src/main/java/com/spyke/gcppubsub/GcpPubSubApplication.java
@@ -0,0 +1,13 @@
+package com.spyke.gcppubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpPubSubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpPubSubApplication.class, args);
+	}
+
+}
---
+++ spring-boot/gcp-pub-sub/src/test/java/com/spyke/gcppubsub/GcpPubSubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spyke.gcppubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpPubSubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
