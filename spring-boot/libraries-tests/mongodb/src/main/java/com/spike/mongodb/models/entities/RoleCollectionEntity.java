6fe2d604382abdd90eb00352287e1f554c1f8e93
fabbo-repo <fscherle98@gmail.com>
2023-11-26 16:19:14 +0100

    update

--- spring-boot/libraries-tests/mongodb/src/main/java/com/spike/mongodb/models/entities/RoleCollectionEntity.java
+++ spring-boot/libraries-tests/mongodb/src/main/java/com/spike/mongodb/models/entities/RoleCollectionEntity.java
@@ -5,11 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldName;
-import org.springframework.data.mongodb.core.mapping.FieldType;
 
 @Document(collection = RoleCollectionEntity.COLLECTION_NAME)
 @Getter
