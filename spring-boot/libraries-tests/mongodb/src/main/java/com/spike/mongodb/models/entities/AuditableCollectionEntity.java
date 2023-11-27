1be1f62727e3d178355033d9c5f233ad09c88b99
fabbo-repo <fscherle98@gmail.com>
2023-11-27 13:05:05 +0100

    update

--- spring-boot/libraries-tests/mongodb/src/main/java/com/spike/mongodb/models/entities/AuditableCollectionEntity.java
+++ spring-boot/libraries-tests/mongodb/src/main/java/com/spike/mongodb/models/entities/AuditableCollectionEntity.java
@@ -12,24 +12,24 @@ import java.time.LocalDateTime;
 @Getter
 public class AuditableCollectionEntity {
 
-    public static final String CREATED_BY_COLUMN_NAME = "createdBy";
-    public static final String CREATED_DATE_COLUMN_NAME = "createdDate";
-    public static final String LAST_MODIFIED_BY_COLUMN_NAME = "lastModifiedBy";
-    public static final String LAST_MODIFIED_DATE_COLUMN_NAME = "lastModifiedDate";
+    public static final String CREATED_BY_FIELD_NAME = "createdBy";
+    public static final String CREATED_DATE_FIELD_NAME = "createdDate";
+    public static final String LAST_MODIFIED_BY_FIELD_NAME = "lastModifiedBy";
+    public static final String LAST_MODIFIED_DATE_FIELD_NAME = "lastModifiedDate";
 
     @CreatedBy
-    @Field(CREATED_BY_COLUMN_NAME)
+    @Field(CREATED_BY_FIELD_NAME)
     private String createdBy;
 
     @CreatedDate
-    @Field(CREATED_DATE_COLUMN_NAME)
+    @Field(CREATED_DATE_FIELD_NAME)
     private LocalDateTime createdDate;
 
     @LastModifiedBy
-    @Field(LAST_MODIFIED_BY_COLUMN_NAME)
+    @Field(LAST_MODIFIED_BY_FIELD_NAME)
     private String lastModifiedBy;
 
     @LastModifiedDate
-    @Field(LAST_MODIFIED_DATE_COLUMN_NAME)
+    @Field(LAST_MODIFIED_DATE_FIELD_NAME)
     private LocalDateTime lastModifiedDate;
 }
