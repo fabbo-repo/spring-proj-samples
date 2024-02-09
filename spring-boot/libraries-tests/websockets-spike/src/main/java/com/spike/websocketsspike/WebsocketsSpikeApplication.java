502f909da441bc7add95d58186a736b395ee1f62
Fabián Scherle <101186527+capf4bbo@users.noreply.github.com>
2024-02-09 17:19:56 +0100

    websockets spike

---
+++ spring-boot/libraries-tests/websockets-spike/src/main/java/com/spike/websocketsspike/WebsocketsSpikeApplication.java
@@ -0,0 +1,13 @@
+package com.spike.websocketsspike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketsSpikeApplication.class, args);
+	}
+
+}
---
+++ spring-boot/libraries-tests/websockets-spike/src/test/java/com/spike/websocketsspike/WebsocketsSpikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spike.websocketsspike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsSpikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
